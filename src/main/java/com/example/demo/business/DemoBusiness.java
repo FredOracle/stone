bb5bd2f895c5666a43d201a53eda972259ab4496
Fred <huxianmiao1985@sina.com>
2020-05-02 21:26:37 +0800

    add commonts

--- src/main/java/com/example/demo/business/DemoBusiness.java
+++ src/main/java/com/example/demo/business/DemoBusiness.java
@@ -1,14 +1,8 @@
 package com.example.demo.business;
 
-import java.util.Collections;
-import java.util.Iterator;
-import java.util.List;
 
 import javax.annotation.Resource;
-
-import org.assertj.core.util.Arrays;
 import org.springframework.stereotype.Service;
-
 import com.example.demo.bean.Demo;
 import com.example.demo.repository.DemoRepository;
 
